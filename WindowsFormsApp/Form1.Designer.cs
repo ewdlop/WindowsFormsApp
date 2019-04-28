253249b4f03355bdfcd8e200ee0859207de94815
Raymond Lei <ray810815@gmail.com>
2019-04-28 01:16:13 -0400

    init

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp
+﻿using System.Drawing;
+
+namespace WindowsFormsApp
 {
     partial class Form1
     {
@@ -28,13 +30,265 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.copyProgressBar = new System.Windows.Forms.ProgressBar();
+            this.copyStatusLabel = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.directoryTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(529, 460);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(29, 499);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 53);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.LoadFromLocalDataBase);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(453, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(572, 576);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 135);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SelectDirectory);
+            // 
+            // copyProgressBar
+            // 
+            this.copyProgressBar.Location = new System.Drawing.Point(17, 269);
+            this.copyProgressBar.Name = "copyProgressBar";
+            this.copyProgressBar.Size = new System.Drawing.Size(321, 23);
+            this.copyProgressBar.Step = 1;
+            this.copyProgressBar.TabIndex = 2;
+            // 
+            // copyStatusLabel
+            // 
+            this.copyStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyStatusLabel.Location = new System.Drawing.Point(17, 168);
+            this.copyStatusLabel.Name = "copyStatusLabel";
+            this.copyStatusLabel.Size = new System.Drawing.Size(321, 87);
+            this.copyStatusLabel.TabIndex = 1;
+            this.copyStatusLabel.Text = "Test";
+            this.copyStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 135);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OpenDirectory);
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(17, 473);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(321, 26);
+            this.directoryTextBox.TabIndex = 4;
+            this.directoryTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 135);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Xls/Xlsm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Xls_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.directoryTextBox);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.copyStatusLabel);
+            this.groupBox1.Controls.Add(this.copyProgressBar);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(78, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 576);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1052, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripMenuItem.Image")));
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripMenuItem.Image")));
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveAsToolStripMenuItem.Image")));
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.toolToolStripMenuItem.Text = "Tool";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::WindowsFormsApp.Properties.Resources.Doge;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1052, 622);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar copyProgressBar;
+        private System.Windows.Forms.Label copyStatusLabel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox directoryTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+
+        public ContentAlignment MiddleCenter { get; private set; }
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +22,270 @@ namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection cnn;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Xls_Click(object sender, EventArgs e)
+        {
+            string promptValue = UserInputDialog("Please enter number of lines per cut:", "Hello");
+
+            if (promptValue != "Bad Input")
+            {
+                string sumbmissionFilePath;
+                int cuts = int.Parse(promptValue);
+
+                OpenFileDialog openSubmissionXlsFileDialog = new OpenFileDialog
+                {
+                    Title = "Open Execl File",
+                    Filter = "Excel Files|*.xls",
+                    InitialDirectory = @"D:\AspNet\WindowsFormsApp\New folder\"
+                };
+
+                OpenFileDialog openSubmissionXlsmFileTenmplateDialog = new OpenFileDialog
+                {
+                    Title = "Open Execl File",
+                    Filter = "Excel Files|*.xlsm",
+                    InitialDirectory = @"D:\AspNet\WindowsFormsApp\New folder\"
+                };
+
+                if (openSubmissionXlsFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        FileStream submissionFile = (FileStream)openSubmissionXlsFileDialog.OpenFile();
+                        sumbmissionFilePath = submissionFile.Name;
+                        HSSFWorkbook xlsWorkBook = new HSSFWorkbook(submissionFile);
+                        ISheet submissionSourceSheet1 = xlsWorkBook.GetSheetAt(0);
+                        int totalLines = submissionSourceSheet1.LastRowNum;
+                        this.copyProgressBar.Maximum = totalLines;
+                        int numberOfCuts = totalLines / cuts + 1;
+                        submissionFile.Close();
+                        if (openSubmissionXlsmFileTenmplateDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            try
+                            {
+                                int startingRow = 1;
+                                int endingRow = Math.Min(cuts, totalLines);
+                                int partCount = 1;
+                                FileStream submissionpPartTemplate = (FileStream)openSubmissionXlsmFileTenmplateDialog.OpenFile();
+
+                                for (partCount = 1; partCount <= numberOfCuts; partCount++)
+                                {
+                                    this.copyStatusLabel.Text = "Copying Part " + partCount;
+                                    this.copyStatusLabel.Refresh();
+                                    CutsAndMakeCopyies(sumbmissionFilePath, submissionpPartTemplate, partCount, startingRow, endingRow);
+                                    //await CutsAndMakeCopyies(sumbmissionFilePath, submissionpPartTemplate, partCount, startingRow, endingRow);
+                                    startingRow += cuts;
+
+                                    if (partCount + 1 == numberOfCuts)
+                                        endingRow = totalLines;
+                                    else
+                                        endingRow += cuts;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Xlsm file openinig issue. Error: " + ex.Message + " " + ex.ToString());
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Xls file openinig issue. Error: " + ex.Message + " " + ex.ToString());
+                    }
+                }
+                this.copyStatusLabel.Text = "test";
+                Process.Start(@"D:\AspNet\WindowsFormsApp\New folder\");
+            }
+        }
+
+        private void CutsAndMakeCopyies(String sumbmissionFilePath, FileStream submissionTemplate, int partCount, int startingRow, int endingRow )
+        {
+            int xlsmRowOfffset = 5;
+            int xlsmColOffset = 1;
+
+            try
+            {
+                FileStream newXlsm = new FileStream(@"D:\AspNet\WindowsFormsApp\New folder\output-p" + partCount + ".xlsm", FileMode.Create, FileAccess.ReadWrite);
+                //await submissionTemplate.CopyToAsync(newXlsm);
+                submissionTemplate.CopyTo(newXlsm);
+                submissionTemplate.Position = 0;
+                newXlsm.Close();
+                try
+                {
+                    newXlsm = new FileStream(@"D:\AspNet\WindowsFormsApp\New folder\output-p" + partCount + ".xlsm", FileMode.Open, FileAccess.ReadWrite);
+                    XSSFWorkbook xlsmWorkBook = new XSSFWorkbook(newXlsm);
+                    newXlsm.Close();
+                    FileStream sumbmissionXlsFile = new FileStream(sumbmissionFilePath, FileMode.Open, FileAccess.Read);
+                    HSSFWorkbook xlsWorkBook = new HSSFWorkbook(sumbmissionXlsFile);
+                    ISheet newXlsmSheet1 = xlsmWorkBook.GetSheet("Garden & Patio");
+                    ISheet submissionSourceSheet1 = xlsWorkBook.GetSheetAt(0);
+                    int lastSumbissionCellNum = submissionSourceSheet1.GetRow(1).LastCellNum;
+                    newXlsm = new FileStream(@"D:\AspNet\WindowsFormsApp\New folder\output-p" + partCount + ".xlsm", FileMode.Create, FileAccess.Write);
+                    try
+                    {
+                        for (int submissionRow = startingRow; submissionRow <= endingRow; submissionRow++)
+                        {
+                            UpdateProgressBar();
+                            for (int submissionCol = 1; submissionCol <= lastSumbissionCellNum; submissionCol++)
+                            {
+                                if (submissionSourceSheet1.GetRow(submissionRow).GetCell(submissionCol) != null)
+                                {
+                                    ICell copyingCell = submissionSourceSheet1.GetRow(submissionRow).GetCell(submissionCol);
+                                    IRow pastingRow = newXlsmSheet1.GetRow(submissionRow - startingRow + xlsmRowOfffset + 1) ?? newXlsmSheet1.CreateRow(submissionRow - startingRow + xlsmRowOfffset + 1);
+                                    ICell pastingCell = pastingRow.GetCell(submissionCol + xlsmColOffset) ?? pastingRow.CreateCell(submissionCol + xlsmColOffset);
+                                    switch (copyingCell.CellType)
+                                    {
+                                        case CellType.Boolean:
+                                            pastingCell.SetCellValue(copyingCell.BooleanCellValue);
+                                            break;
+                                        case CellType.Numeric:
+                                            pastingCell.SetCellValue(copyingCell.NumericCellValue);
+                                            break;
+                                        case CellType.String:
+                                            pastingCell.SetCellValue(copyingCell.StringCellValue);
+                                            break;
+                                    }
+                                    
+                                }
+                            }
+                        }
+                        xlsmWorkBook.Write(newXlsm);
+                        newXlsm.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Copying rows to new xlsm file issue. Error: " + ex.Message + " " + ex.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Copied files opening issue D. Error: " + ex.Message + " " + ex.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Somethign went wrong with copying the xlsm. Error: " + ex.Message + " " + ex.ToString());
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            this.copyProgressBar.PerformStep();
+            this.copyProgressBar.Refresh();
+        }
+      
+
+        public string UserInputDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 200 };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 200 };
+            Button OK = new Button() { Text = "Ok", Left = 100, Width = 100, Top = 80, DialogResult = DialogResult.None};
+
+            
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(OK);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = OK;
+
+
+            OK.Click += (sender, e) => {
+
+                Match match = Regex.Match(textBox.Text, @"^[1-9][0-9]*$");
+                if (match.Success)
+                {
+                    prompt.DialogResult = DialogResult.OK;
+                    prompt.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Something wrong with the input");
+                }
+            };
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "Bad Input";
+        }
+
+        private void SelectDirectory(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string path = fbd.SelectedPath;
+                    this.directoryTextBox.Text = path;
+                }
+            }
+        }
+
+        private void OpenDirectory(object sender, EventArgs e)
+        {
+            try
+            {
+                if(directoryTextBox.TextLength > 0)
+                {
+                    Process.Start(directoryTextBox.Text);
+                }
+            
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void LoadFromLocalDataBase(object sender, EventArgs e)
+        {
+            string connetionString = ConfigurationManager.ConnectionStrings["WindowsFormsApp.Properties.Settings.masterConnectionString"].ConnectionString;
+
+            try
+            {
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.PurchaseOrderDetail", cnn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cnn.Close(); 
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
+
 }
